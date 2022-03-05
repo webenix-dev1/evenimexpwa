5fd8f4885fd732300313374044420a60d324a1b2
mit <mitp@webenix.net>
2022-03-05 17:33:53 +0530

    small change

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import Layout from "../components/Layout";
 import { GlobalStyle } from "../styles/global/global.style";
 import { ThemeProvider } from "styled-components";
 import theme from "../styles/global/theme";
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 function MyApp({ Component, pageProps }) {
