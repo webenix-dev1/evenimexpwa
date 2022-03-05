810213893b3981b3ecea65897186b062c62681ea
mit <mitp@webenix.net>
2022-03-05 17:31:14 +0530

    small change

--- pages/_document.js
+++ pages/_document.js
@@ -39,20 +39,20 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
-          {/* <link
+          <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
             integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-            crossOrigin="anonymous"
-          /> */}
+            
+          />
           <link
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
             rel="stylesheet"
           />
           <script href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/js/all.min.js"></script>
           {/* <script src="https://unpkg.com/react/umd/react.production.min.js"></script>
-          <script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"></script>
-          <script src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"></script> */}
+          <script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"></script> */}
+          <script src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"></script>
         </Head>
         <body>
           <Main />
