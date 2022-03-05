a10a6d1b14e149442257703759039edf517a06c6
mit <mitp@webenix.net>
2022-03-05 17:46:17 +0530

    dsd

--- pages/_document.js
+++ pages/_document.js
@@ -31,32 +31,29 @@ class MyDocument extends Document {
       <Html>
     <Head>
           <meta property="og:title" content="Evenimex" />
-          <link href="fonts/style.css" rel="stylesheet"></link>
+          {/* <link href="fonts/style.css" rel="stylesheet"></link>
           <link rel="icon" href="favicon.ico" />
+          <link href="fonts/stylesheet.css" rel="stylesheet"></link>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
-         
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
             integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-           
+            crossOrigin="anonymous"
           />
-            
-          
           <link
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
             rel="stylesheet"
           />
           <script href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/js/all.min.js"></script>
-          {/* <script src="https://unpkg.com/react/umd/react.production.min.js"></script>
-          <script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"></script> */}
-          <script src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"></script>
-         
+          <script src="https://unpkg.com/react/umd/react.production.min.js"></script>
+          <script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"></script>
+          <script src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"></script> */}
         </Head>
         <body>
           <Main />
