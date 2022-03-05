2ee596402e3dff4bf2ccb8cbe73c313c3edd7d07
mit <mitp@webenix.net>
2022-03-05 17:08:23 +0530

    small change

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const path = require("path");
 
-module.exports = nextConfig
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  
+};
