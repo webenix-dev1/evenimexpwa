7f1a74163d9a3d062ffe26421195eac151e07f8f
mit <mitp@webenix.net>
2022-03-05 17:56:44 +0530

    small

--- pages/_document.js
+++ pages/_document.js
@@ -50,14 +50,17 @@ class MyDocument extends Document {
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
             rel="stylesheet"
           />
-          <script href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/js/all.min.js"></script>
-         
-          <script src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"></script>
-          <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
+       <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
 
 <script
   src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
   crossorigin></script>
+
+<script
+  src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
+  crossorigin></script>
+
+<script>var Alert = ReactBootstrap.Alert;</script>
         </Head>
         <body>
           <Main />
