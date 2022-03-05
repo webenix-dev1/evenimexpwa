a3c61be0b4f4ca0a07c98ac7c03fe1ce8059c288
mit <mitp@webenix.net>
2022-03-05 17:58:32 +0530

    asasa

--- pages/_document.js
+++ pages/_document.js
@@ -50,17 +50,7 @@ class MyDocument extends Document {
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
             rel="stylesheet"
           />
-       <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
-
-<script
-  src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
-  crossorigin></script>
-
-<script
-  src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
-  crossorigin></script>
-
-<script>var Alert = ReactBootstrap.Alert;</script>
+      
         </Head>
         <body>
           <Main />
